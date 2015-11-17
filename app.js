c6fa7911361975c3a331fa1d0ce36f6c0ad1ede8
Thomas Bisignani <contact@thomasbisignani.com>
2015-11-17 18:11:51 +0100

    Add comment and update syntax app

--- app.js
+++ app.js
@@ -5,11 +5,12 @@ var fs = require('fs');
 var app = express();
 
 // Create stream
-var accessLogStream = fs.createWriteStream(__dirname + '/access.log', {flags: 'a'})
+var accessLogStream = fs.createWriteStream(__dirname + '/access.log', {flags: 'a'});
 
 // Setup and use the logger
 app.use(morgan('common', {stream: accessLogStream}))
 
+// Routes HTTP requests
 app.get('/', function(req, res) {
     res.setHeader('Content-Type', 'text/plain');
     res.end('Home');
@@ -19,7 +20,7 @@ app.get('/foo', function(req, res) {
     res.end('bar');
 });
 
-app.use(function(req, res, next){
+app.use(function(req, res, next) {
     res.setHeader('Content-Type', 'text/plain');
     res.status(404).send('Page not found');
 });
